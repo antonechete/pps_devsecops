64e784c6611c1ec8887630f8897ada93da5dee73
Administrador <administrador@eqXX>
2026-05-29 16:30:54 +0200

    Feat: Ejercicio 1

---
+++ ejercicio1/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>¡Hola desde mi contenedor seguro de Docker!</h1>";
+echo "<p>Hora del servidor: " . date('Y-m-d H:i:s') . "</p>";
+phpinfo(); // Esto queda muy profesional para la captura de pantalla
+?>
